eb4951551b5432e8e04c14775df14e1717ec9740
Misaki <q096385@gmail.com>
2024-06-04 21:48:51 +0900

    Revert "Add Health system. Update Texture designer"
    This reverts commit 49ccc4eea7fa4663c497f5ac7fe6733754069d45.

--- Runtime/Node/NodeLibrary/Input/BitmapSource.cs
+++ Runtime/Node/NodeLibrary/Input/BitmapSource.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 namespace TextureDesigner
@@ -20,33 +18,5 @@ namespace TextureDesigner
         public SingleChannelTextureSocket B;
         [NodeOutput]
         public SingleChannelTextureSocket A;
-
-        public override bool AssignInput(Dictionary<int, Socket> input)
-        {
-            return true;
-        }
-
-        public override Socket[] Process()
-        {
-            Output = new TextureSocket(Texture.width, Texture.height);
-            R = new SingleChannelTextureSocket(Texture.width, Texture.height);
-            G = new SingleChannelTextureSocket(Texture.width, Texture.height);
-            B = new SingleChannelTextureSocket(Texture.width, Texture.height);
-            A = new SingleChannelTextureSocket(Texture.width, Texture.height);
-
-            var computeShaderPath = ComputeShaderPath.GetPath("Input/BitmapSource");
-            var computeShader = AssetDatabase.LoadAssetAtPath<ComputeShader>(computeShaderPath);
-
-            var kernel = computeShader.FindKernel("CSMain");
-            computeShader.SetTexture(kernel, "Input", Texture);
-            computeShader.SetTexture(kernel, "Output", Output.Texture);
-            computeShader.SetTexture(kernel, "R", R.Texture);
-            computeShader.SetTexture(kernel, "G", G.Texture);
-            computeShader.SetTexture(kernel, "B", B.Texture);
-            computeShader.SetTexture(kernel, "A", A.Texture);
-            computeShader.Dispatch(kernel, Texture.width / 8, Texture.height / 8, 1);
-
-            return new Socket[] { Output, R, G, B, A };
-        }
     }
 }
